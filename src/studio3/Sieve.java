66d971a0ef425dc302ef661f658c9b72b3915cf9
emmayin <emmayin@10.232.178.139>
2022-09-22 14:19:11 -0500

    studio3

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -1,5 +1,36 @@
 package studio3;
 
+import java.util.Scanner;
+
 public class Sieve {
 
-}
+	Scanner in = new Scanner (System.in);
+	int n = in.nextInt();
+int[] prime = new int [n+1];
+	
+
+	for (int i = 2; i < n+1 ; i++)
+		prime [i]=i;
+		System.out.println(prime[1]);
+		for (prime[]% 2 = 0) {
+			int prime = 0;
+		}
+				
+			
+			
+			
+	
+	
+	
+//	
+//	for (int blah % 2 != 0;) {
+//		for (blah % 3 != 0) {
+//			for (blah % 5 != 0) {
+//				for (blah % 7 != 0) 
+//			}
+//		}
+//	}
+}	
+}	
+	
+
